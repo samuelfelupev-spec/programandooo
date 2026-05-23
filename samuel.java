fc9485009550b6418c8ab43ceb87d86f7ee55aae
samueluco <samuelfelupev@gmail.com>
2026-05-22 21:06:02 -0500

    Soy el mas lindo

---
+++ samuel.java
@@ -0,0 +1,9 @@
+public class samuel {
+    public static void main(String[] args) {
+        String name = "malparido hijueputa";
+    
+    
+        System.out.println("nombre: " + name);
+        
+    }
+}
